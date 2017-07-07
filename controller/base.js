7bb6a30326fc73c314c4c8edd1a18066459fe2bb
ruiiur <2362641334@qq.com>
2017-07-07 11:10:24 +0800

    router

--- controller/base.js
+++ controller/base.js
@@ -9,19 +9,19 @@ angular.module('myApp').controller('baseCtrl',['$scope',function ($scope) {
     // $scope.changeMenuStatus=function(){
     //    $scope.isShow=!$scope.isShow;
     // }
-    console.log($scope.text);
-    jQuery('ul').css('background','red');
-    jQuery('.setting').on('click',function(){
-        alert();
-        $scope.isShow=!$scope.isShow;
-    });
-    $('body').not(".setting").on('click',function(){
-        if ($scope.isShow == false){
-            alert(222);
-            return;
-        }else{
-            $scope.isShow = false;
-        }
-        // $scope.$apply();
-    })
+    // console.log($scope.text);
+    // jQuery('ul').css('background','red');
+    // jQuery('.setting').on('click',function(){
+    //     alert();
+    //     $scope.isShow=!$scope.isShow;
+    // });
+    // $('body').not(".setting").on('click',function(){
+    //     if ($scope.isShow == false){
+    //         alert(222);
+    //         return;
+    //     }else{
+    //         $scope.isShow = false;
+    //     }
+    //     // $scope.$apply();
+    // })
 }]);
--- router.js
+++ router.js
@@ -10,66 +10,36 @@ angular.module('myApp').config(['$stateProvider','$urlRouterProvider','$ionicCon
     $ionicConfigProvider.platform.android.views.transition('android');
 
     //路由配置
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/table');
     $stateProvider
         //桌台页
         .state('table', {
-            url: '/',
-            views:{
-                'table':{
-                    templateUrl: "views/table/table.html"
-                    // controller:'orderCtrl'
-                }
-            }
+            url: '/table',
+            templateUrl: "views/table/table.html"
         })
         //点菜页
         .state('order', {
             url: 'order',
-            views:{
-                'order':{
-                    templateUrl: "views/order/order.html"
-                    // controller:'orderCtrl'
-                }
-            }
+            templateUrl: "views/order/order.html"
         })
         //结账页
         .state('pay', {
             url: 'pay',
-            views:{
-                'pay':{
-                    templateUrl: "views/pay/pay.html"
-                    // controller:'orderCtrl'
-                }
-            }
+            templateUrl: "views/pay/pay.html"
         })
         //送餐页
         .state('meals', {
             url: 'meals',
-            views:{
-                'meals':{
-                    templateUrl:'views/meals/meals.html'
-                    // controller:'orderCtrl'
-                }
-            }
+            templateUrl:'views/meals/meals.html'
         })
         //订单页
         .state('indent', {
             url: 'indent',
-            views:{
-                'indent':{
-                    templateUrl: "views/indent/indent.html"
-                    // controller:'orderCtrl'
-                }
-            }
+            templateUrl: "views/indent/indent.html"
         })
         //统计页
         .state('statistical', {
             url: 'statistical',
-            views:{
-                'statistical':{
-                    templateUrl: "views/statistical/statistical.html"
-                    // controller:'orderCtrl'
-                }
-            }
+            templateUrl: "views/statistical/statistical.html"
         })
 }]);
