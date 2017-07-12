8142d44965152b69dff0b71852efaa2e455a8e2f
liangjiang <xuan03121994@163.com>
2017-07-12 17:26:58 +0800

    base edit

--- controller/base.js
+++ controller/base.js
@@ -8,6 +8,10 @@ angular.module('myApp').controller('baseCtrl',['$scope',function ($scope) {
     $scope.isShow = false;
     $scope.changeMenuStatus = function () {
         $scope.isShow = !$scope.isShow;
-        $('.setting').attr('background-color','#35b6d98');
+        if($scope.isShow){
+            $('.setting').css({'backgroundColor':'#5b6d98','backgroundImage':'url(images/subnav-list.png)'});
+        }else{
+            $('.setting').css({'backgroundColor':'','backgroundImage':'url(images/HeaderDropdownIcon.png)'});
+        }
     }
 }]);
